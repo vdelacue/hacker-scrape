af0407ad238c6270f6e247df6babafaf31349f07
Vanessa de La Cuetara <23455324+vdelacue@users.noreply.github.com>
2019-08-22 21:15:04 -0400

    deplyed to heroku

--- public/assets/javascript/index.js
+++ public/assets/javascript/index.js
@@ -48,6 +48,26 @@ $.getJSON("/articles", function(data) {
         }
       });
   });
+
+  $(document).on("click", "#newScrape", function() {
+    // Empty the notes from the note section
+    $("#notes").empty();
+    // Save the id from the p tag
+    var thisId = $(this).attr("data-id");
+  
+    // Now make an ajax call for the Article
+    $.ajax({
+      method: "GET",
+      url: "/scrape" 
+    })
+      // With that done, add the note information to the page
+      .then(function(data) {
+        console.log(data);
+        // The title of the article
+      
+      });
+  });
+  
   
   // When you click the savenote button
   $(document).on("click", "#savenote", function() {
